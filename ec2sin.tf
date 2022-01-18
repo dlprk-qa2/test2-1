338d746021497142a20d35ea5eee38d19149a0ee
dlprk-qa1 <97515913+dlprk-qa1@users.noreply.github.com>
2022-01-18 19:54:05 +0530

    Update ec2sin.tf

--- ec2sin.tf
+++ ec2sin.tf
@@ -19,11 +19,36 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.10.0.0/16"
 }
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   ingress_cidr_blocks = ["0.0.0.0/16"]
   tags = {
     Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  description = "controls access to the application ELB"
+
+  vpc_id = aws_vpc.main.id
+  name   = "tf-ecs-lbsg"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
   }
 }
